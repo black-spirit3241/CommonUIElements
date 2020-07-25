e6ecae63031a0b5dbbd5af8a1c3a2f0e2317b44b
black-spirit3241 <blackspirit781863@gmail.com>
2020-07-26 00:50:38 +0630

    BottomAppBar

--- app/src/main/java/com/kotlin/commonuielements/MainActivity.kt
+++ app/src/main/java/com/kotlin/commonuielements/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.kotlin.commonuielements
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        bottomAppBar.replaceMenu(R.menu.main_menu)
+       // til_another_flying_hint.setError("Some Error")
+
+        fab.setOnClickListener {
+
+            Snackbar.make(it,"Show me something",Snackbar.LENGTH_INDEFINITE)
+                .setAction("Ok", null)
+                .show()
+
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+
+
+
 }
